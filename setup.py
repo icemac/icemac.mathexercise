344b946174a17391da9f72d73848de6d34b04b32
Michael Howitz <mh@gocept.com>
2014-02-01 14:34:43 +0100

    Initial working version

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 gocept gmbh & co. kg
+# Copyright (c) 2014 Michael Howitz
 # See also LICENSE.txt
 
 # This should be only one line. If it must be multi-line, indent the second
@@ -30,6 +30,7 @@ setup(
         },
 
     entry_points={
+        'console_scripts': 'mathexercise.txt=icemac.mathexercise.main:txt',
         },
 
     author='gocept <mail@gocept.com>',
